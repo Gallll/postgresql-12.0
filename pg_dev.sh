30445c031306ef3ab0dc0e31dec956e48d341cf0
coo <1960990826@qq.com>
2022-08-31 09:39:52 +0800

    fix: complete pg_dev.sh

--- pg_dev.sh
+++ pg_dev.sh
@@ -3,4 +3,16 @@
 --enable-debug \
 --with-openssl \
 --enable-depend CFLAGS=-O0 \
---prefix /home/coo/postgresql-12.0/
+--prefix /home/coo/postgresql-12.0/ \
+--enable-license \
+--with-perl \
+--enable-BCT \
+--enable-rpath \
+--with-ossp-uuid \
+--with-zlib \
+--with-libxslt \
+--with-python \
+--with-ldap \
+--enable-FDE \
+LDFLAGS="-llber" \
+
